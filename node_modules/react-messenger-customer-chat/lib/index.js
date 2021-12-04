8023e314cd1675a3e1739efa77f79805a0a5920a
João Lima <joaolimafonseca2@gmail.com>
2021-12-04 12:46:26 -0300

    remove node modules

--- node_modules/react-messenger-customer-chat/lib/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _MessengerCustomerChat = require('./MessengerCustomerChat');
-
-var _MessengerCustomerChat2 = _interopRequireDefault(_MessengerCustomerChat);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = _MessengerCustomerChat2.default;
