a8e233656bc421188e66ce291517ee7d02831259
andersonalvescoelho <a0a0coelho0@gmail.com>
2022-03-15 22:38:16 -0300

    (Syle)[logo] Troca de logo

--- components/headers/common/logo.js
+++ components/headers/common/logo.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 const LogoImage = ({ logo }) => {
     return (
-        <Fragment>
+        <>
             <Link href={'/'} >
                 <a>
-                    <img src={`/assets/images/icon/${logo?logo:'logo.png'}`} alt="" className="img-fluid" />
+                    <img src={`/assets/images/icon/${logo?logo:'logo.png'}`} alt="" className="width-logo" />
                 </a>
             </Link>
-        </Fragment>
+        </>
     )
 }
 
